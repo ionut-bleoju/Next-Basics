20936ed914e2284239db2828ef418f4c1bf590c1
ionutyop <ionutyop@gmail.com>
2020-06-15 10:46:27 +0300

    Added different handlers for  http requests

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,47 +2,52 @@ import { compare } from 'bcrypt'
 import db from '../../lib/db'
 import { sign } from 'jsonwebtoken'
 import cookie from 'cookie'
+import secrete from '../../lib/secret'
 
 const ONE_HOUR = 60 * 60 * 1;
 
 export default async (req, res) => {
-  const { email, password } = req.body
-  if (!email || !password) {
-    res.status(400)
-    return
-  }
-
-  try {
-    const { rows } = await db('SELECT id, email, hash from "user" where email=$1', [email])
-
-    if (rows.length > 0) {
-      const isEqual = await compare(password, rows[0].hash)
-      if (isEqual) {
-
-        const token = sign({
-          sub: rows.id,
-          email: rows.email
-        }, 'someSupeRsECRET', { expiresIn: ONE_HOUR })
-
-        res.setHeader('Set-Cookie', cookie.serialize('authentication', token,
-          {
-            httpOnly: true,
-            secure: false,
-            sameSite: 'strict',
-            maxAge: ONE_HOUR,
-            path: '/'
-          }))
-
-        res.status(200).json({ message: 'Authentication successful' })
-        return
-      }
+  if (req.method === 'POST') {
+    const { email, password } = req.body
+    if (!email || !password) {
+      res.status(400)
+      return
     }
 
-    res.status(400).json({ message: 'Email or password are invalid!' })
+    try {
+      const { rows } = await db('SELECT id, email, hash from "user" where email=$1', [email])
 
-  } catch (error) {
-    console.error(error)
-    res.status(500)
+      if (rows.length > 0) {
+        const isEqual = await compare(password, rows[0].hash)
+        if (isEqual) {
+
+          const token = sign({
+            sub: rows.id,
+            email: rows.email
+          }, secrete, { expiresIn: ONE_HOUR })
+
+          res.setHeader('Set-Cookie', cookie.serialize('authentication', token,
+            {
+              httpOnly: true,
+              secure: false,
+              sameSite: 'strict',
+              maxAge: ONE_HOUR,
+              path: '/'
+            }))
+
+          res.status(200).json({ message: 'Authentication successful' })
+          return
+        }
+      }
+
+      res.status(400).json({ message: 'Email or password are invalid!' })
+
+    } catch (error) {
+      console.error(error)
+      res.status(500)
+    }
+  } else {
+    res.status(405).json({message: 'Methode not supported'})
   }
 
 }
--- pages/api/signin.js
+++ pages/api/signin.js
@@ -2,28 +2,32 @@ import bcrypt from 'bcrypt'
 import db from '../../lib/db'
 
 export default async (req, res) => {
-  const { email, password, confirm } = req.body;
-  if (password != confirm) {
-    res.status(400).json({ message: "Password don't match!" })
-
-    return
-  }
-
-  try {
-    const { rows: entries } = await db('SELECT id, email, hash from "user" where email=$1', [email])
-
-    if (entries.length > 0) {
-      res.status(409).json({ message: "User already exists" })
+  if (req.methode === 'POST') {
+    const { email, password, confirm } = req.body;
+    if (password !== confirm) {
+      res.status(400).json({ message: "Password don't match!" })
 
       return
     }
 
-    const hash = await bcrypt.hash(password, 10)
-    await db('INSERT INTO "user" (email, hash) VALUES($1,$2)', [email, hash])
+    try {
+      const { rows: entries } = await db('SELECT id, email, hash from "user" where email=$1', [email])
 
-    res.status(200).json({ message: 'User created successful' })
-  } catch (error) {
-    res.status(500)
-    console.error(error)
+      if (entries.length > 0) {
+        res.status(409).json({ message: "User already exists" })
+
+        return
+      }
+
+      const hash = await bcrypt.hash(password, 10)
+      await db('INSERT INTO "user" (email, hash) VALUES($1,$2)', [email, hash])
+
+      res.status(200).json({ message: 'User created successful' })
+    } catch (error) {
+      res.status(500)
+      console.error(error)
+    }
+  } else {
+    res.status(405).json({ message: 'Methode not supported' })
   }
 }
