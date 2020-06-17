d2ca453d0861e16db7c0e45fed3c340ad6f4be85
ionutyop <ionutyop@gmail.com>
2020-06-17 18:21:29 +0300

    Added individual pokemon

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,6 +1,8 @@
 import React from 'react'
-import PokemonCard from '../../components/pokemon-card'
 import { getPokemon } from '../../lib/pokemon'
+import Layout from '../../components/layout'
+import styles from '../../styles/pokemon.module.scss'
+import { Row, Col } from 'antd';
 
 
 class Pokemon extends React.Component {
@@ -12,21 +14,48 @@ class Pokemon extends React.Component {
       console.log(error);
     }
 
-    console.log(pokemon)
-
     return {
       id: pokemon.id,
       name: pokemon.name,
       stats: pokemon.stats,
       types: pokemon.types,
-      sprites: pokemon.sprites
     }
   }
 
   render() {
+    const availableStats = ['Health', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']
+
     return (
-      <div>
-        <PokemonCard {...this.props} />
+      <div className={styles.background}>
+        <Layout >
+          <div className={styles.container} >
+            <img alt="pokemon" src={`https://pokeres.bastionbot.org/images/pokemon/${this.props.id}.png`} />
+            <div className={styles.info}>
+              <h1>{this.props.name}</h1>
+              <div className={styles.types}>
+                {this.props.types.map((type) =>
+                  <div className={`${styles.icon} ${styles[type.type.name]}`} key={type.type.name}>
+                    <img src={`/types/${type.type.name}.svg`} />
+                  </div>
+                )}
+              </div>
+              <hr style={{ height: "1px" }} />
+
+              <ul className={styles.stats}>
+                {
+                  this.props.stats.map((stat, index) => {
+                    return (
+                      <Row key={stat.stat.name}>
+                        <Col span={8} offset={4}>{availableStats[index]}</Col>
+                        <Col span={8} offset={4}>{stat.base_stat}</Col>
+                      </Row>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+        </Layout>
       </div>
     )
   }
